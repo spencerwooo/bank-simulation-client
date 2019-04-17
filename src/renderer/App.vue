46aa0a76b7e53d6be0d56f34090736ecf87ff5b7
spencerwooo <spencerwoo98@gmail.com>
2019-04-17 12:06:39 +0800

    🌟Init client framework

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'bank-simulation-client'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
